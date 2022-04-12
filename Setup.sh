e1843c3a926eb2c7ebe13aa42793f3b029bb6c8e
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2022-04-12 08:40:00 +0800

    Dev 1.0.2 Setup command support for MacOS added

--- Setup.sh
+++ Setup.sh
@@ -2,6 +2,6 @@
 chcp 65001
 title Setup Apex Hot Key
 echo 正在安裝程式所需的模組 Installing modules
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 echo 程式運作中 Program is running
 py Main.py
