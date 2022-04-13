80ed18085d434a91147c0c15da2cfe27e988bd7c
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2022-04-13 10:20:01 +0800

    Dev 1.0.3 Modify automate typing procedure
    After much experimenting, found Apex Legends unable to detect automate press "enter", so user must to manually open the chat, then press automate typing hotkey.

--- src/Automate.py
+++ src/Automate.py
@@ -2,6 +2,5 @@ import pyautogui as auto
 
 class Auto:
     def auto_typing(msg):
-        auto.press("enter")
-        auto.typewrite(msg) 
+        auto.typewrite(msg)
         auto.press("enter")
